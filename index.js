a464a02fe6e54bd7c8e210055b346497fa696e57
Dahlia <36625739+MahiPalihawadana@users.noreply.github.com>
2021-07-23 22:39:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: "30mb" , extended:true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.qjz7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://Mahi:Mahi123@cluster0.qjz7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology: true})
